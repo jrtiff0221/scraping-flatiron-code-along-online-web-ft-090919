aa1cf0a788c7e310707ac12e57a9f50288f9ad83
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-30 17:40:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,19 +5,7 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
- 
-    doc.css(".post").each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-    end
-  end
 
- 
-  Scraper.new.get_page
 
 
 
