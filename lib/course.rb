3e36f69db6418bb4f58594ca83dbf568cbc40b4d
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-30 17:26:47 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -16,3 +16,13 @@ class Course
     @@all.clear
   end
 end
+  #def print_courses
+   # self.make_courses
+    #Course.all.each do |course|
+     # if course.title && course.title != ""
+      #  puts "Title: #{course.title}"
+       # puts "  Schedule: #{course.schedule}"
+        #puts "  Description: #{course.description}"
+      #end
+    #end
+  #end
