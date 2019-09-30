9fc47c38e55bfd6ebbb98adf5793d963278c281d
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-30 17:25:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,19 +16,8 @@ class Scraper
 end
  
   Scraper.new.get_page
-end
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-  
-end
+
+
 
 
 
